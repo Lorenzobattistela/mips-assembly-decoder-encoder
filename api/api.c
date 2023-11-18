455bd1da4f082124914cc934384b63a55a03d1a6
Lorenzobattistela <lorenzobattistela@gmail.com>
2023-11-18 10:14:48 -0300

    [feat] cors support + options

--- api/api.c
+++ api/api.c
@@ -6,15 +6,25 @@
 #include <string.h>
 #include <stdbool.h>
 
-#define PORT 8080
+#define PORT 5000
 // compile with 
 // gcc $(pkg-config --cflags libulfius) -o api api.c ../encoder.c ../io.c ../main.c ../decoder.c ../helpers.c  $(pkg-config --libs libulfius) -I ..
+
 
 bool validateHexadecimalInstruction(char *hexadecimal) {
   if(strlen(hexadecimal) != 10 || strncmp(hexadecimal, "0x", 2) != 0) {
     return false;
   }
   return true;
+}
+
+int callback_options(const struct _u_request *request, struct _u_response *response, void *user_data)
+{
+  u_map_put(response->map_header, "Access-Control-Allow-Origin", "*");
+  u_map_put(response->map_header, "Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+  u_map_put(response->map_header, "Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Bearer, Authorization");
+  u_map_put(response->map_header, "Access-Control-Max-Age", "1800");
+  return U_CALLBACK_COMPLETE;
 }
 
 int callback(const struct _u_request *request, struct _u_response *response, void *user_data) {
@@ -45,7 +55,6 @@ int decodeCallback(const struct _u_request *request, struct _u_response *respons
   while((line = getNextLine(out))) {
     strcat(res, line);
   }
-
   ulfius_set_string_body_response(response, 200, res);
   return U_CALLBACK_CONTINUE;
 }
@@ -71,16 +80,18 @@ int encodeCallback(const struct _u_request *request, struct _u_response *respons
 
 int main(void) {
   struct _u_instance instance;
-
   if(ulfius_init_instance(&instance, PORT, NULL, NULL) != U_OK) {
     fprintf(stderr, "Error ulfius_init_instance, abort\n");
     return(1);
   }
 
-  // endpoint list declaration
+  u_map_put(instance.default_headers, "Access-Control-Allow-Origin", "*");
+
+  // endpoint list declaration 
+  ulfius_add_endpoint_by_val(&instance, "OPTIONS", NULL, "*", 0, &callback_options, NULL);
   ulfius_add_endpoint_by_val(&instance, "GET", "/helloworld", NULL, 0, &callback, NULL);
   ulfius_add_endpoint_by_val(&instance, "POST", "/decode", NULL, 0, &decodeCallback, NULL);
-  ulfius_add_endpoint_by_val(&instance, "POST", "/encode", NULL, 9, &encodeCallback, NULL);
+  ulfius_add_endpoint_by_val(&instance, "POST", "/encode", NULL, 0, &encodeCallback, NULL);
 
   if(ulfius_start_framework(&instance) == U_OK) {
     printf("Start framework on port %d\n", PORT);
